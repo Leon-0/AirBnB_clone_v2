3ed755aa55ae445a99de62dc33105daa43d8af47
Jesuferanmi Omoniyi <lyoncbe@gmail.com>
2023-06-21 18:53:40 +0100

    test

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
 """ """
 from tests.test_models.test_base_model import test_basemodel
-from models.review import Review
+from models.state import State
 
 
-class test_review(test_basemodel):
+class test_state(test_basemodel):
     """ """
 
     def __init__(self, *args, **kwargs):
         """ """
         super().__init__(*args, **kwargs)
-        self.name = "Review"
-        self.value = Review
+        self.name = "State"
+        self.value = State
 
-    def test_place_id(self):
+    def test_name3(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.place_id), str)
-
-    def test_user_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.user_id), str)
-
-    def test_text(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.text), str)
+        self.assertEqual(type(new.name), str)
